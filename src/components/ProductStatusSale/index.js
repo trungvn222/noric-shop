6c859b14ae8fc83c9b164c6591d9b7bd709a8ce3
QTrung <ngoquangtrung.it@gmail.com>
2018-09-25 00:35:08 +0700

    no message

--- src/components/ProductStatusSale/index.js
+++ src/components/ProductStatusSale/index.js
@@ -5,7 +5,7 @@ class ProductStatusSale extends PureComponent {
     render() {
         const {discount} = this.props;
         return (
-            <div className="product_bubble product_bubble_left product_bubble_green d-flex flex-column align-items-center"><span>{ discount ? <Currency price={discount} /> : 'sale' }</span></div>
+            <div className="product_bubble product_bubble_right product_bubble_red d-flex flex-column align-items-center"><span>-{ discount ? <Currency price={discount} /> : 'sale' }</span></div>
         );
     }
 }
